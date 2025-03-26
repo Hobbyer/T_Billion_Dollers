d6b2f70b613c4f45cbb9b049197dfd9c3361651b
onghyeya <hhw3960@gmail.com>
2025-03-26 16:04:29 +0900

    admin main layout 진행중

--- React/farm_animals_shop/src/admin_component/livestock_management/LiveStockTemperature.jsx
+++ React/farm_animals_shop/src/admin_component/livestock_management/LiveStockTemperature.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
 const LiveStockTemperature = () => {
   //환경 온도 그래프
   return (
-    <div>온도 그래프 : 바 형태</div>
+    <div>
+      온도 그래프 : 선 형태
+      <img src="" alt="" />
+    </div>
   )
 }
 
