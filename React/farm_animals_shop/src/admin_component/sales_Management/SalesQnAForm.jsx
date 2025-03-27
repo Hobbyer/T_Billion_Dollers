a8dbb5ab755285c4467619aa183c45b51da706c7
1uuuuuuuuuuuuuuumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-03-27 17:49:52 +0900

    aa

--- React/farm_animals_shop/src/admin_component/sales_Management/SalesQnAForm.jsx
+++ React/farm_animals_shop/src/admin_component/sales_Management/SalesQnAForm.jsx
@@ -1,41 +1,76 @@
-import React from 'react'
+import React from "react";
+import styles from "../Admin.module.css";
+import { useNavigate } from "react-router-dom";
 
 // Q&A 등록 게시판
 const SalesQnAForm = () => {
+  const nav = useNavigate();
+
   return (
-    <>
-      <div>Q&A</div>
-      {/* 질문 제목 */}
-      <p>제목</p>
+    <div className={styles.form_container}>
+      <h2>Q&A 등록하기</h2>
+      <table className={styles.form_table}>
+        <colgroup>
+          <col style={{width:"20%"}} />
+          <col style={{width:"80%"}} />
+        </colgroup>
+        <tbody>
+          {/* 글 제목 */}
+          <tr className={styles.form_title}>
+            <td>제목
+              <span className={styles.required}>*</span>
+            </td>
+            <td>
+              <input type="text" placeholder="제목을 입력해 주세요" />
+            </td>
+          </tr>
 
-      {/* 작성자 */}
-      <div>
-        <span>작성자</span>
-        <span>홍혜원</span>
+          {/* 작성자 */}
+          <tr className={styles.form_userId}>
+            <td>작성자
+              <span className={styles.required}>*</span>
+            </td>
+            <td>
+              <input type="text" placeholder="이름을 입력해 주세요" />
+            </td>
+          </tr>
+
+          {/* 설명 */}
+          <tr className={styles.form_content}>
+            <td>설명
+              <span className={styles.required}>*</span>
+            </td>
+            <td>
+              <input type="text" placeholder="설명을 입력해 주세요" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      {/* 첨부파일 */}
+      <table className={styles.form_file}>
+        <colgroup>
+          <col style={{width:"20%"}} />
+          <col style={{width:"80%"}} />
+        </colgroup>
+        <tr>
+          <td>
+          <span>파일 첨부</span>
+          </td>
+          <td>
+            <label for="input-file">이곳을 클릭해 첨부파일을 업로드 하세요</label>
+            <input type="file" id="input-file" style={{display:"none"}} />
+          </td>
+        </tr>
+      </table>
+
+      {/* 등록, 목록 버튼 */}
+      <div className={styles.form_btn}>
+        <button type="button" onClick={() => nav("/admin/sales-questions")}>목 록</button>
+        <button type="button">등 록</button>
       </div>
+    </div>
+  );
+};
 
-      {/* 작성일 */}
-      <div>
-        <span>작성일 </span>
-        <span>2025.03.25 17:38</span>
-      </div>
-
-      {/* 조회 */}
-      <div>
-        <span>조회</span>
-        <span>13</span>
-      </div>
-
-      {/* 설명 */}
-      <p>설명</p>
-
-      {/* 버튼 클릭 시 질문 리스트 목록으로 이동 */}
-      <button type='button'>목록</button>
-
-      {/* 버튼 클릭 시 수정하는 페이지로 이동 */}
-      <button type="button">수정하기</button>
-    </>
-  )
-}
-
-export default SalesQnAForm
+export default SalesQnAForm;
