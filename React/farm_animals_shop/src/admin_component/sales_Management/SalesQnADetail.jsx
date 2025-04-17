954555bf1dc80f332b46c5363768363edfe772f5
1uuuuuuuuuuuuuuumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-04-17 16:17:37 +0900

    리액트 : 회원가입의 아이디 중복체크중 / 스프링 : 답글쿼리까지 작성

--- React/farm_animals_shop/src/admin_component/sales_Management/SalesQnADetail.jsx
+++ React/farm_animals_shop/src/admin_component/sales_Management/SalesQnADetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import {
   Container,
@@ -8,6 +8,10 @@ import {
   Form,
   FormLabel,
   FormText,
+  Accordion,
+  Card,
+  useAccordionButton,
+  AccordionContext,
 } from "react-bootstrap";
 
 const baseURL = import.meta.env.VITE_API_URL;
@@ -19,6 +23,25 @@ const SalesQnADetail = () => {
   const { questionNum } = useParams();
   const location = useLocation(); //이전 페이지에서 전달된 상태를 가져오기 위한 useLocation 훅 사용
 
+  //로딩 상태를 관리하기 위한 state 변수
+  const [isOpen, setIsOpen] = useState(false);
+  const [answer, setAnswer] = useState("");   // 답변 내용 관리
+
+   // 아코디언 열기/닫기 상태 토글
+   const handleAccordionToggle = () => {
+    setIsOpen(!isOpen); // 상태 반전
+  };
+
+    // 답변 내용 관리
+    const handleAnswerChange = (e) => {
+      setAnswer(e.target.value);
+    };
+
+  const handleSubmit = () => {
+    // 여기서 서버로 답변을 보내는 로직을 추가
+    alert("답변이 서버로 전송되었습니다!");
+  };
+
   const [loading, setLoading] = useState(true); //로딩 상태를 관리하기 위한 state 변수
 
   //조회한 상세 정보를 저장할 state 변수
@@ -29,7 +52,43 @@ const SalesQnADetail = () => {
 
   //해당 게시글의 답글 목록을 저장할 state 변수
 
-  
+  const ContextAwareToggle = ({ children, eventKey, callback }) => {
+    const { activeEventKey } = useContext(AccordionContext);
+
+    const decoratedOnClick = useAccordionButton(
+      eventKey,
+      () => callback && callback(eventKey)
+    );
+
+    const isCurrentEventKey = activeEventKey === eventKey;
+
+    //서버에서 답변 목록을 가져오는 로직
+    useEffect(() => {
+      GET(`${baseURL}/questions/${questionNum}`)
+        .then((res) => {
+          setQuestion(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    }, []);
+
+    return (
+      <button
+        type="button"
+        className={
+          isCurrentEventKey
+            ? "css-button-fully-rounded--yellow"
+            : "css-button-fully-rounded--green"
+        }
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </button>
+    );
+  };
 
   useEffect(() => {
     //서버에서 게시글 상세 정보 받아오기
@@ -42,21 +101,21 @@ const SalesQnADetail = () => {
         console.log(err);
         setLoading(false); //로딩 완료
       });
-  }, [])
+  }, []);
 
   const deleteQuestion = () => {
-    if(window.confirm("정말 삭제하시겠습니까?")){
+    if (window.confirm("정말 삭제하시겠습니까?")) {
       DELETE(`${baseURL}/questions/${questionNum}`)
-      .then(() => {
-        alert("삭제되었습니다.");
-        nav("/admin/sales-questions");
-      })
-      .catch((err) => {
-        console.log(err);
-        alert("삭제에 실패했습니다.");
-      })
+        .then(() => {
+          alert("삭제되었습니다.");
+          nav("/admin/sales-questions");
+        })
+        .catch((err) => {
+          console.log(err);
+          alert("삭제에 실패했습니다.");
+        });
     }
-  }
+  };
 
   return (
     <>
@@ -66,50 +125,92 @@ const SalesQnADetail = () => {
           from { transform: rotate(0deg); }
           to { transform: rotate(360deg); }
           }
+
+          .css-button-fully-rounded--yellow {
+            min-width: 130px;
+            height: 35px;
+            font-size: 14px;
+            color: #FEB904;
+            padding: 5px 10px;
+            font-weight: bold;
+            cursor: pointer;
+            transition: all 0.3s ease;
+            position: relative;
+            display: inline-block;
+            outline: none;
+            border-radius: 20px;
+            border: 2px solid #FEB904;
+            background: #fff;
+          }
+
+          .css-button-fully-rounded--green {
+            min-width: 130px;
+            height: 35px;
+            font-size: 14px;
+            color: #32B67A;
+            padding: 5px 10px;
+            font-weight: bold;
+            cursor: pointer;
+            transition: all 0.3s ease;
+            position: relative;
+            display: inline-block;
+            outline: none;
+            border-radius: 20px;
+            border: 2px solid #32B67A;
+            background: #fff;
+          }
+
+          textarea {
+            resize: none;
+            width: 100%;
+            height: 100px;
         `}
       </style>
 
-      { loading ? 
+      {loading ? (
         <div style={{ padding: "0 100px" }}>
           <h3>Loading...</h3>
-          <div className='d-flex justify-content-center align-items-center'>
-          <img
-            className='loading-img mt-3'
-            src="/imgs/cow (1).png" // 원하는 로딩 이미지 경로
-            alt="로딩중"
-            style={{
-              width: "60px",
-              height: "60px",
-              animation: "spin 1s linear infinite"
-            }}
-          />
+          <div className="d-flex justify-content-center align-items-center">
+            <img
+              className="loading-img mt-3"
+              src="/imgs/cow (1).png" // 원하는 로딩 이미지 경로
+              alt="로딩중"
+              style={{
+                width: "60px",
+                height: "60px",
+                animation: "spin 1s linear infinite",
+              }}
+            />
           </div>
         </div>
-        :
+      ) : (
         <Container
-        style={{
-          border: "none",
-          margin: "0",
-          padding: "0",
-          display: "flex",
-          justifyContent: "center"
-        }}
-      >
+          style={{
+            border: "none",
+            margin: "0",
+            padding: "0",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
           <Form style={{ padding: "30px", width: "100%", maxWidth: "600px" }}>
-
             {/* 제목 */}
-            <Form.Group className="mb-3" controlId="formTitle" style={{marginBottom: "20px", borderBottom: "1px solid #000"}}>
+            <Form.Group
+              className="mb-3"
+              controlId="formTitle"
+              style={{ marginBottom: "20px", borderBottom: "1px solid #000" }}
+            >
               <FormText
-                style={{ fontWeight: "bold", fontSize: "24px", color: "#000"}}
+                style={{ fontWeight: "bold", fontSize: "24px", color: "#000" }}
               >
-                {location.state?.title || question.title || "제목이 없습니다."} 
+                {location.state?.title || question.title || "제목이 없습니다."}
               </FormText>
             </Form.Group>
 
             {/* 작성자 */}
             <Form.Group className="mb-3" controlId="formAuthor">
-              <FormLabel style={{ fontWeight: "bold", fontSize: "20px", }}>
-              작성자
+              <FormLabel style={{ fontWeight: "bold", fontSize: "20px" }}>
+                작성자
               </FormLabel>
               <FormText
                 style={{
@@ -118,7 +219,9 @@ const SalesQnADetail = () => {
                   marginBottom: "10px",
                 }}
               >
-                {location.state?.userId || question.userId || "작성자 정보 없음"}
+                {location.state?.userId ||
+                  question.userId ||
+                  "작성자 정보 없음"}
               </FormText>
             </Form.Group>
 
@@ -131,20 +234,39 @@ const SalesQnADetail = () => {
                 style={{
                   display: "block",
                   fontSize: "16px",
-                  marginBottom: "10px",
+                  marginBottom: "70px",
                 }}
               >
                 {question.content || "내용이 없습니다."}
               </FormText>
             </Form.Group>
+
+{/* 답변 */}
+<Accordion defaultActiveKey="0">
+              <Card>
+                <Card.Header>
+                  <ContextAwareToggle eventKey="0">답변하기</ContextAwareToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <Form.Control
+                      as="textarea"
+                      placeholder="답변을 입력해주세요."
+                    />
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+
+            {/* 삭제 버튼 */}
             <div className="d-flex justify-content-end mt-5">
               <Button type="button" variant="danger" onClick={deleteQuestion}>
-                  삭제
+                삭제
               </Button>
             </div>
           </Form>
         </Container>
-      }
+      )}
     </>
   );
 };
--- React/farm_animals_shop/src/web_component/UserSignup.jsx
+++ React/farm_animals_shop/src/web_component/UserSignup.jsx
@@ -29,11 +29,14 @@ const UserSignup = () => {
     phoneNumber: "",
     phoneCheck: "",
     address: "",
+    firstAddress: "",
+    detailedAddress: "",
     isAgreed: false,
   });
 
   const [codeSent, setCodeSent] = useState(false); // 인증번호 발송 여부
   const [isPopupOpen, setIsPopupOpen] = useState(false); // 팝업창 열기 여부
+  const [isUserIdValid, setIsUserIdValid] = useState(false); // 아이디 중복 여부
 
   // 주소 검색 팝업창 열기
   const openPostCode = () => {
@@ -50,7 +53,10 @@ const UserSignup = () => {
     setFormData((prevData) => ({
       ...prevData,
       firstAddress: data.roadAddress, // 선택된 주소를 formData에 저장
-      address: data.roadAddress,
+    }));
+    setValidation((prevValidation) => ({
+      ...prevValidation,
+      firstAddress: data.roadAddress.length > 0, // 주소가 들어갔는지 확인
     }));
     closePostCode(); // 모달 닫기
   };
@@ -62,14 +68,47 @@ const UserSignup = () => {
       [name]: type === "checkbox" ? checked : value,
     };
 
+    // 주소가 입력되면 유효성 검사 추가
+    if (name === "firstAddress") {
+      // 첫번째 주소가 비어있지 않으면 유효성 검증 성공
+      setValidation((prevValidation) => ({
+        ...prevValidation,
+        firstAddress: value.length > 0, // 주소가 들어갔는지 확인
+      }));
+    }
+
     updatedFormData.email = `${updatedFormData.emailFirst}${updatedFormData.emailSecond}`;
     updatedFormData.address = `${updatedFormData.firstAddress} ${updatedFormData.detailedAddress}`;
 
     setFormData(updatedFormData);
   };
 
+  // 중복확인 버튼 클릭 시 아이디 중복 확인
+  const validateUserId = () => {
+    if (!formData.userId.trim()) {
+      alert("아이디를 입력해주세요.");
+      return; // 아이디가 비어있으면 중복확인 요청을 보내지 않음
+    }
+
+    axios
+      .post(`${baseURL}/auth/check-user-id`, { userId: formData.userId})
+      .then((res) => {
+        if (res.data.exists){
+          alert("이미 사용중인 아이디입니다.");
+          setIsUserIdValid(false);
+        } else {
+          alert("사용 가능한 아이디입니다.");
+          setIsUserIdValid(true);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("아이디 중복 확인에 실패했습니다.");
+      });
+  };
+
   const signupSubmit = (e) => {
-    if (formData.isAgreed === true) {
+    if (formData.isAgreed === true && isUserIdValid) {
       axios
         .post(`${baseURL}/auth/signup`, formData)
         .then((res) => {
@@ -129,6 +168,8 @@ const UserSignup = () => {
     name: false,
     emailFirst: false,
     phoneNumber: false,
+    firstAddress: false,
+    detailedAddress: false,
   });
 
   const idRegex = /^[a-z]{4,12}$|^[a-z0-9]{4,12}$/; // 4~12자 영문 소문자, 숫자 조합
@@ -137,6 +178,7 @@ const UserSignup = () => {
   const nameRegex = /^[가-힣]{2,8}$/; // 2~8자 한글
   const emailFirstRegex = /^[a-zA-Z0-9._%+-]{4,15}$/; // 이메일 형식
   const phoneRegex = /^(?:\d{3}-\d{4}-\d{4}|\d{11})$/; // 010-1234-5678 or 01012345678
+  const detailedAddressRegex = /^[가-힣0-9\s]+$/; // 주소 형식 (한글, 숫자, 공백 포함)
 
   const tagRegex = (name, value) => {
     if (name === "userId") {
@@ -149,6 +191,8 @@ const UserSignup = () => {
       return emailFirstRegex.test(value);
     } else if (name === "phoneNumber") {
       return phoneRegex.test(value);
+    } else if (name === "detailedAddress") {
+      return detailedAddressRegex.test(value);
     }
   };
 
@@ -190,7 +234,6 @@ const UserSignup = () => {
     return Object.values(newErrors).every(Boolean); // 모든 유효성 검사 통과 여부
   };
 
-  // 주소 검색
 
   return (
     <>
@@ -198,7 +241,7 @@ const UserSignup = () => {
         className="mt-5 mb-5 px-5 d-flex justify-content-center align-items-center"
         style={{ minHeight: "100vh" }}
       >
-        <div style={{ width: "100%", maxWidth: "600px" }}>
+        <div style={{ width: "500px" }}>
           <div className="text-center mb-5">
             <Image
               src="/imgs/animal.png"
@@ -212,25 +255,34 @@ const UserSignup = () => {
             <h1>Farmdas</h1>
           </div>
 
+              {/* 아이디 */}
           <Form className="w-100">
             <Form.Label>아이디</Form.Label>
-            <Form.Group className="mb-2" controlId="formGridEmail">
-              <Form.Control
-                type="text"
-                placeholder="User ID"
-                name="userId"
-                value={formData.userId}
-                onChange={(e) => {
-                  handleChange(e);
-                  tagCheck(e);
-                }}
-                isValid={validation.userId && formData.userId.length > 0}
-                isInvalid={!validation.userId && formData.userId.length > 0}
-              />
-              <Form.Control.Feedback type="invalid" className="px-3">
-                4~12자 영문 소문자, 숫자 조합
-              </Form.Control.Feedback>
-            </Form.Group>
+              <Row className="mb-2">
+                <Col xs={9}>
+                  <Form.Control
+                    type="text"
+                    placeholder="ID"
+                    name="userId"
+                    value={formData.userId}
+                    onChange={(e) => {
+                      handleChange(e);
+                      tagCheck(e);
+                    }}
+                    isValid={validation.userId && formData.userId.length > 0}
+                    isInvalid={!validation.userId && formData.userId.length > 0}
+                  />
+                </Col>
+                <Col xs={3} className="d-flex justify-content-end">
+                  <Button variant="outline-success" type="button" style={{ width: "100px"}} onClick={validateUserId}>
+                    중복확인
+                  </Button>
+                </Col>
+              
+                <Form.Control.Feedback type="invalid" className="px-3">
+                  4~12자 영문 소문자, 숫자 조합
+                </Form.Control.Feedback>
+              </Row>
 
             <Row className="mb-2">
               <Form.Group as={Col} controlId="formGridPassword">
@@ -322,9 +374,9 @@ const UserSignup = () => {
                     handleChange(e);
                     tagCheck(e);
                   }}
-                  isValid={validation.emailFirst && formData.email.length > 0}
+                  isValid={validation.emailFirst && formData.emailFirst.length > 0}
                   isInvalid={
-                    !validation.emailFirst && formData.email.length > 0
+                    !validation.emailFirst && formData.emailFirst.length > 0
                   }
                 />
               </Form.Group>
@@ -348,7 +400,7 @@ const UserSignup = () => {
             <Row className="mb-2">
               <Form.Label>연락처</Form.Label>
 
-              <Form.Group as={Col} controlId="formGridPhone">
+              <Form.Group xs={9} as={Col} controlId="formGridPhone">
                 <Form.Control
                   type="text"
                   placeholder="010-1234-5678"
@@ -370,13 +422,14 @@ const UserSignup = () => {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} xs="auto">
+              <Form.Group xs={3} as={Col} className="d-flex justify-content-end">
                 <Button
                   variant="success"
                   type="button"
                   onClick={sendVerificationCode}
+                  style={{ width: "100%"}}
                 >
-                  인증번호 발송
+                  인증번호
                 </Button>
               </Form.Group>
             </Row>
@@ -385,7 +438,7 @@ const UserSignup = () => {
             {codeSent && (
               <Row className="mb-2">
                 {codeCompleted === false ? (
-                  <Form.Group as={Col} controlId="formGridPhoneCheck">
+                  <Form.Group xs={9} as={Col} controlId="formGridPhoneCheck">
                     <Form.Control
                       type="text"
                       placeholder="인증번호"
@@ -395,7 +448,7 @@ const UserSignup = () => {
                     />
                   </Form.Group>
                 ) : (
-                  <Form.Group as={Col} controlId="formGridPhoneCheck">
+                  <Form.Group xs={9} as={Col} controlId="formGridPhoneCheck">
                     <Form.Control
                       disabled={true}
                       type="text"
@@ -405,7 +458,7 @@ const UserSignup = () => {
                     />
                   </Form.Group>
                 )}
-                <Form.Group as={Col} xs="auto">
+                <Form.Group xs={3} as={Col} className="d-flex justify-content-end">
                   <Button variant="success" type="button" onClick={verifyCode}>
                     확인
                   </Button>
@@ -416,16 +469,22 @@ const UserSignup = () => {
             {/* 주소 검색 + 상세주소 입력창 */}
             <Row className="mb-2">
               <Form.Label>주소</Form.Label>
-              <Col xs={8}>
+              <Col xs={9}>
                 <Form.Control
                   placeholder="주소를 입력해주세요."
                   name="firstAddress"
                   value={formData.firstAddress}
-                  onChange={handleChange}
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
                   readOnly
+                  isValid={validation.firstAddress}
+                  isInvalid={
+                    !validation.firstAddress && formData.address.length > 0
+                  }
                 />
               </Col>
-              <Col xs="auto">
+              <Col xs={3} className="d-flex justify-content-end">
                 <Button variant="outline-success" onClick={openPostCode}>
                   주소찾기
                 </Button>
@@ -461,7 +520,18 @@ const UserSignup = () => {
               <Form.Control
                 name="detailedAddress"
                 value={formData.detailedAddress}
-                onChange={handleChange}
+                onChange={(e) => {
+                  handleChange(e);
+                  tagCheck(e); // 상세주소 유효성 검사
+                }}
+                isValid={
+                  validation.detailedAddress &&
+                  formData.detailedAddress.length > 0
+                }
+                isInvalid={
+                  !validation.detailedAddress &&
+                  formData.detailedAddress.length > 0
+                }
               />
             </Form.Group>
 
@@ -491,6 +561,8 @@ const UserSignup = () => {
                   !validation.name ||
                   !validation.emailFirst ||
                   !validation.phoneNumber ||
+                  !validation.firstAddress ||
+                  !validation.detailedAddress ||
                   !formData.isAgreed ||
                   !codeCompleted
                 }
