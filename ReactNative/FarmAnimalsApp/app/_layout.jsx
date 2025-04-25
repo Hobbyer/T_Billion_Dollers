1ff1c9b423643a7440806871831e87ae8045091f
onghyeya <hhw3960@gmail.com>
2025-04-25 14:56:05 +0900

    native: 앱 실행시 로그인 화면부터 구현

--- ReactNative/FarmAnimalsApp/app/_layout.jsx
+++ ReactNative/FarmAnimalsApp/app/_layout.jsx
@@ -1,10 +1,26 @@
 // app/_layout.jsx
 
-import React from "react";
-import { Stack, Tabs } from "expo-router";
+import React, { useEffect } from "react";
+import { Stack, Tabs, useRouter } from "expo-router";
 import { SafeAreaView, StatusBar } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function RootLayout() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // 앱 시작 시 로그인 여부를 체크
+    const checkLoginStatus = async () => {
+      const accessToken = await AsyncStorage.getItem('accessToken');
+      
+      if (!accessToken) {
+        // 로그인하지 않은 경우 로그인 화면으로 리디렉션
+        router.push('/auth/login'); // 로그인 화면으로 이동
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }} />
