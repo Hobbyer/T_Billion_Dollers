68d60e808594e2b2f5d73f3e210c575f43335dde
onghyeya <hhw3960@gmail.com>
2025-04-28 10:08:43 +0900

    최신 데이터를 위한 commit

---
+++ ReactNative/FarmAnimalsApp/app/(tabs)/settings/account.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+// 관리자 계정 생성 및 권한 부여
+const account = () => {
+  return (
+    <View>
+      <Text>account</Text>
+    </View>
+  )
+}
+
+export default account
+
+const styles = StyleSheet.create({})
